a4eeb51e3061aff6e1d51960eca507dfc89b6cc9
Debanil1996 <debanilguha96@gmail.com>
2024-09-08 12:50:45 +0200

    Added Config yaml

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'SalaryChecker',
-  webDir: 'www'
+  appId: 'com.example.app',
+  appName: 'salary-checker',
+  webDir: 'www',
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    }
+  }
 };
 
 export default config;
